61fc729687b64bdc87628c3c178b3931557618f0
aryadubey1 <120629785+aryadubey1@users.noreply.github.com>
2026-02-19 21:19:11 +0530

    Change ALLOWED HOST

--- tuition_site/settings.py
+++ tuition_site/settings.py
@@ -27,8 +27,8 @@ SECRET_KEY = 'django-insecure-y%k(t8!@5a762=lttu&+3mu&x22me9h82gn=8^radhvoife6y6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
-
+ALLOWED_HOSTS = ['rcc-amn6.onrender.com','localhost', '127.0.0.1']
+CSRF_TRUSTED_ORIGINS = ['https://rcc-amn6.onrender.com']
 
 # Application definition
 
